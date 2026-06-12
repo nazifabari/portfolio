723d274c25c70194b2c1ce92df34e202d2d938fa
nazifa <nazifabari@gmail.com>
2026-06-12 13:27:39 -0400

    initial React and Tailwind setup, built Intro component

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import Intro from './components/Intro'
+
+function App() {
+  return (
+ <Intro /> 
+ )
+}
+export default App
---
+++ src/components/Intro.tsx
@@ -0,0 +1,17 @@
+import profilePhoto from '../assets/profile.jpg'
+function Intro() {        
+  return ( 
+
+<div>
+  <h1 >Nazifa Bari</h1> 
+
+ <img src={profilePhoto} alt="profile image" />
+
+  <p>Hi I am Nazifa! I am a Computer Science student at the University at Buffalo.</p>
+
+</div>
+
+  )
+}
+
+export default Intro
